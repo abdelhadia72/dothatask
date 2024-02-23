580d3654d016a7b48db671e56480b55b1d04ee81
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-23 19:37:02 +0100

    Update Signin component styles

--- frontend/src/components/Auth/Signin.jsx
+++ frontend/src/components/Auth/Signin.jsx
@@ -8,14 +8,14 @@ function Signin() {
     return (
         <div className="w-full flex  relative bg-neutral-100">
             <Logo />
-            <div className="lg:w-[612px] md:w-[550px] w-[290px] m-auto mt-[173px] inline-flex flex-col items-center md:gap-[20px] gap-[25px] p-[35px] relative bg-white rounded-md">
+            <div className="lg:w-[612px] md:w-[650px] w-[300px] m-auto mt-[173px] inline-flex flex-col items-center md:gap-[20px] gap-[25px] p-[35px] relative bg-white rounded-md">
                 <div className="text-stone-950 md:text-5xl text-xl font-bold md:mb-[40px]">Sign in</div>
                 <form className="flex-col justify-center items-center md:gap-10 gap-5 flex">
                     <div className="flex-col justify-start items-start gap-[15px] flex">
                         <Inputs placeholder={"email adress"} inpType={"text"} />
                         <Inputs placeholder={"Password"} inpType={"password"} />
                     </div>
-                    <div className="justify-start items-center md:gap-[195px] lg:gap-[238px] gap-3 inline-flex mb-[-40px]">
+                    <div className="justify-start items-center md:gap-[195px] lg:gap-[238px] gap-4 inline-flex mb-[-40px]">
                         <div className="justify-start items-center md:gap-[9px] gap-1 flex">
                             <input type='checkbox' className="md:w-6 md:h-6 w-3 h-3 relative" />
                             <p className="text-left text-neutral-600 md:text-lg text-sm font-normal">Remember me</p>
