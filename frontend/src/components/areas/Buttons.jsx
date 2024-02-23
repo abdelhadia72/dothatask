e1f7ec93ad3dac5e599f2786eb3d203c3a056507
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-23 16:18:26 +0100

    Update Signin component styling and add responsive design

--- frontend/src/components/areas/Buttons.jsx
+++ frontend/src/components/areas/Buttons.jsx
@@ -4,11 +4,11 @@ function Buttons({ content }) {
     return (
         <>
             <div
-                className="w-[542px] h-12 p-2.5 hover:bg-emerald-400 bg-emerald-600 rounded-[5px] 
+                className="md:w-[542px] w-[500px] h-12 p-2.5 hover:bg-emerald-400 bg-emerald-600 rounded-[5px] 
                             shadow-lg justify-center items-center gap-[113px] 
                             inline-flex mt-[40px] transition-all 
                             duration-100 ease-in-out cursor-pointer shadow-gray-100 ">
-                <button className="text-white text-2xl font-medium font-['Ubuntu']">{content}</button>
+                <button className="text-white md:text-2xl text-xl font-medium font-['Ubuntu']">{content}</button>
             </div>
         </>
     )
