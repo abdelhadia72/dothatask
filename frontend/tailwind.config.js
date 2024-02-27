da4594c98529814886346c1a51315877ce37dc26
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-27 16:46:58 +0100

    Add hover gray text color to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -37,6 +37,7 @@ module.exports = {
         do_green: '#00B05E',
         do_hover_green: '#008A4B',
         do_gray_text: '#6C6C6C',
+        do_hover_gray_text: '#b8b8b8',
         do_gray_text_input: '#909090',
         do_white: 'F7F7F7',
         border: "hsl(var(--border))",
