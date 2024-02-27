b2273e1b1e79c6e04a24935e396ef1dac506caf9
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-27 16:47:25 +0100

    Add new routes for Services and Homepage

--- frontend/src/components/routes/Routes.jsx
+++ frontend/src/components/routes/Routes.jsx
@@ -3,13 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 import Signin from '../Auth/Signin';
 import Signup from '../Auth/Signup';
 import ForgotPassword from '../Auth/ForgotPassword';
+import Services from '../pages/Services';
+import Homepage from '../pages/Homepage';
 
 const Routers = () => {
   return (
     <Routes>
-        <Route path="/login" element={<Signin/>} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/forgotpassword" element={<ForgotPassword/>} />
+      <Route path="/" element={<Homepage />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/login" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/forgotpassword" element={<ForgotPassword />} />
     </Routes>
   );
 };
