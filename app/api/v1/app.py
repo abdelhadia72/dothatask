6e3536f1659f8a8180f0a23f570448f1babd6e6b
Abdeljalil37 <widadpop@gmail.com>
2024-03-02 00:32:25 +0100

    Refactor app.py to use blueprint for API versioning

--- app/api/v1/app.py
+++ app/api/v1/app.py
@@ -1,16 +1,17 @@
 from flask import Flask, jsonify
-from models.review import Review
-from models.db import DBManager
+from api.v1.views import app_v1
+
 
 app = Flask(__name__)
+app.register_blueprint(app_v1)
 
 
-@app.route('/app')
-def get_workers():
-    db_ss = DBManager()
-    data = db_ss.show(Review)
-    sdata = [el.to_dict() for el in data]
-    return jsonify(sdata)
+# @app.route('/app', strict_slashes=False)
+# def get_workers():
+#     db_ss = DBManager()
+#     data = db_ss.show(Review)
+#     sdata = [el.to_dict() for el in data]
+#     return jsonify(sdata)
 
 
 if __name__ == '__main__':
