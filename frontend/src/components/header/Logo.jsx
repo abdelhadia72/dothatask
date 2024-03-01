1e0665771f8b087e4ed6f6e1b09c76512458cdbd
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-01 16:55:00 +0100

    Add link to logo in header

--- frontend/src/components/header/Logo.jsx
+++ frontend/src/components/header/Logo.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import logo from "/logo.png";
+import {Link} from "react-router-dom";
 
 export const Logo = () => {
   return (
-    <div className=" flex gap-2 items-center">
+    <Link to={'/'} className=" flex gap-2 items-center">
       <img className="max-w-[47px]" src={logo} alt="logo.png" />
       <p className="text-3xl text-do_green font-abril font-500 hidden sm:block">
         Dothatask
       </p>
-    </div>
+    </Link>
   );
 };
