85e034f059f81313c34f6fa1daaabe3e59817534
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-03 14:28:17 +0100

    Refactor category links in Footer component

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -9,8 +9,8 @@ function Footer() {
                 <div className="text-do_green ml-5 md:mx-[120px] mx-[70px] md:text-[50px] text-[30px] font-normal font-abril">Dothatask</div>
                 <div className='flex flex-col gap-2 mt-[-20px] mb-[97px] mr-[50px]'>
                     <div className="text-neutral-800 md:text-2xl text-[20px] font-medium font-Ubuntu">Categories</div>
-                    {categories.map((e) =>
-                        <a className="text-do_gray_text hover:do_hover_gray_text md:text-[22px] text-[17px] cursor-pointer font-medium font-Ubuntu">{e}</a>
+                    {categories.map((e,idx) =>
+                        <a key={idx} className="text-do_gray_text hover:do_hover_gray_text md:text-[22px] text-[17px] cursor-pointer font-medium font-Ubuntu">{e}</a>
                     )}
                 </div>
             </div>
