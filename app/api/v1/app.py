1852b5bc5137b19dfb7a729a757fd62c082fec0e
Abdeljalil37 <widadpop@gmail.com>
2024-03-04 17:34:56 +0100

    Enable debug mode in app.run()

--- app/api/v1/app.py
+++ app/api/v1/app.py
@@ -6,13 +6,5 @@ app = Flask(__name__)
 app.register_blueprint(app_v1)
 
 
-# @app.route('/app', strict_slashes=False)
-# def get_workers():
-#     db_ss = DBManager()
-#     data = db_ss.show(Review)
-#     sdata = [el.to_dict() for el in data]
-#     return jsonify(sdata)
-
-
 if __name__ == '__main__':
-    app.run(port=5000, host="0.0.0.0", debug=False)
+    app.run(port=5000, host="0.0.0.0", debug=True)
