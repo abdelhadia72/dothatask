58bcfe7e5e3f1ef97c1a26f00d5106adc457f713
Abdeljalil37 <widadpop@gmail.com>
2024-03-04 17:38:23 +0100

    Refactor Worker model to inherit from Base and BaseModle

--- app/models/workers.py
+++ app/models/workers.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 from sqlalchemy import Column, Integer, String, Text, ForeignKey
 from models.users import User
+from models.base_model import Base, BaseModle
 
-class Worker(User):
+class Worker(Base, BaseModle):
     __tablename__ = 'workers'
     title = Column(String(255), nullable=False)
     status = Column(String(20), nullable=False)
     description = Column(Text)
     category_id = Column(Integer, ForeignKey('categories.id'))
+    # image_id should not be in this table
     image_id = Column(Integer, ForeignKey('images.id'))
     user_id = Column(Integer, ForeignKey('users.id'))
 
 
     __mapper_args__ = {
-        'polymorphic_identity': 'worker',
+        'polymorphic_identity': 'workers',
     }
