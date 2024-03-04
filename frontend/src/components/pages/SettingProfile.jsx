1789175bb1093d836bbbadfc6f111c76cf22a675
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-04 20:20:10 +0100

    Refactor SettingProfile component to include Personaldescription component

--- frontend/src/components/pages/SettingProfile.jsx
+++ frontend/src/components/pages/SettingProfile.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Personalinfo from '../settingProfile/Personalinfo'
+import Personaldescription from '../settingProfile/Personaldescription'
 
 function SettingProfile() {
   return (
-    <div className='m-32'>
+    <div className='lg:m-32 m-0'>
         <div className='flex flex-col gap-16'>
-            <div className="text-neutral-800 text-5xl font-bold ">My Profile</div>
-            <Personalinfo />
+            <div className="text-neutral-800 lg:text-5xl md:text-3xl text-2xl font-bold mx-4">My Profile</div>
+            <div className='flex lg:flex-row flex-col gap-14 mx-auto'>
+              <Personalinfo />
+              <Personaldescription />
+            </div>
         </div>
     </div>
   )
