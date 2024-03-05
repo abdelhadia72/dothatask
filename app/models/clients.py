8a1dae2b785123331181401391c240a376c16a65
Abdeljalil37 <widadpop@gmail.com>
2024-03-05 02:09:13 +0100

    Remove commented code in clients.py

--- app/models/clients.py
+++ app/models/clients.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from sqlalchemy import Column, String, Text, Integer, ForeignKey
 from models.base_model import Base, BaseModle
-from models.users import User
+
 
 class Client(Base, BaseModle):
     __tablename__ = 'clients'
@@ -10,7 +10,3 @@ class Client(Base, BaseModle):
     status = Column(String(255), nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'))
     
-
-    # __mapper_args__ = {
-    #     'polymorphic_identity': 'clients',
-    # }
