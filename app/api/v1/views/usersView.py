878f7236dd9ffd3e5de38600a6a9ead4352e9092
Abdeljalil37 <widadpop@gmail.com>
2024-03-05 02:06:23 +0100

    Refactor usersView.py to use db_manager instead of dbm

--- app/api/v1/views/usersView.py
+++ app/api/v1/views/usersView.py
@@ -4,9 +4,7 @@
 from flask import jsonify, request
 from api.v1.views import app_v1, SessionLocal, db_manager
 from models.users import User
-from models.db import DBManager
 
-#dbm = DBManager()
 session = SessionLocal()
 
 
@@ -40,11 +38,10 @@ def create_user():
     return jsonify(get_user.to_dict()), 201
 
 
-
 @app_v1.route('/users', methods=['GET'], strict_slashes=False)
 def get_users():
     """ get all users """
-    data = dd.show(User)
+    data = db_manager.show(User)
     sdata = [el.to_dict() for el in data]
     return jsonify(sdata)
 
@@ -52,7 +49,7 @@ def get_users():
 @app_v1.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def get_user(user_id):
     """ get user by id """
-    user = dbm.get(User, user_id) 
+    user = db_manager.get(User, user_id) 
     if not user:
         return jsonify({"error": "User not found"}), 404
     return jsonify(user.to_dict())
@@ -61,17 +58,17 @@ def get_user(user_id):
 @app_v1.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id):
     """ delete user by id """
-    user = dbm.get(User, user_id)
+    user = db_manager.get(User, user_id)
     if not user:
         return jsonify({"error": "User not found"}), 404
-    dbm.delete(User, user_id)
+    db_manager.delete(User, user_id)
     return jsonify({}), 204
 
 
 @app_v1.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_user(user_id):
     """ update user by id """
-    user = dbm.get(User, user_id)
+    user = db_manager.get(User, user_id)
     if not user:
         return jsonify({"error": "User not found"}), 404
     data = request.get_json()
@@ -79,5 +76,5 @@ def update_user(user_id):
         return jsonify({"error": "Not a JSON"}), 400
     for key, value in data.items():
         setattr(user, key, value)
-    dbm.update(User, user_id, data)
+    db_manager.update(User, user_id, data)
     return jsonify(user.to_dict()), 200
