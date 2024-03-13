bfdc1674f0ead11fad2fcc8b64a14ceebe4c5091
abdelhadia72 <thisisavnb@gmail.com>
2024-03-13 13:45:18 +0000

    clear the nav and start the landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,25 +7,26 @@ import Homepage from "./components/pages/Homepage";
 import SearchCategorie from "./components/areas/SearchCategorie";
 import Footer from "./components/footer/Footer";
 import ServiceDetails from "./components/pages/ServiceDetails";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import NavbarLogged from "./components/header/NavbarLogged";
-import {LOGGED} from "./components/Auth/Signin";
+import { LOGGED } from "./components/Auth/Signin";
 
 export const App = () => {
   const [Auth, setAuth] = useState(false);
-  LOGGED ? console.log(LOGGED) : console.log('not logged');
+  LOGGED ? console.log(LOGGED) : console.log("not logged");
 
   // Add logic here
 
   return (
     <div>
-      <div className="z-[1] font-ubuntu">
-         {!LOGGED ?  <Navbar /> : <NavbarLogged />}
+      <div className="z-[1] font-ubuntu ">
+        {!LOGGED ? <Navbar /> : <NavbarLogged />}
       </div>
-      <Routers />
-      <Footer />
-      ,<Link to="/details" className="bg-black w-[50px]"> Details </Link>
-      ,<Link to="/profile" className="bg-black w-[50px]"> profile </Link>
+
+      <div className="flex flex-col justify-between h-screen">
+        <Routers />
+        <Footer />
+      </div>
     </div>
   );
 };
--- frontend/src/components/routes/Routes.jsx
+++ frontend/src/components/routes/Routes.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Signin from '../Auth/Signin';
-import Signup from '../Auth/Signup';
-import ForgotPassword from '../Auth/ForgotPassword';
-import Services from '../pages/Services';
-import Homepage from '../pages/Homepage';
-import ServiceDetails from '../pages/ServiceDetails';
-import { Profilepage } from '../pages/ProfilePage';
-import AddService from '../pages/AddService';
-import SettingProfile from '../pages/SettingProfile';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Signin from "../Auth/Signin";
+import Signup from "../Auth/Signup";
+import ForgotPassword from "../Auth/ForgotPassword";
+import Services from "../pages/Services";
+import Homepage from "../pages/Homepage";
+import ServiceDetails from "../pages/ServiceDetails";
+import { Profilepage } from "../pages/ProfilePage";
+import AddService from "../pages/AddService";
+import SettingProfile from "../pages/SettingProfile";
+import { Landing } from "../pages/Landing";
 
 const Routers = () => {
   return (
     <Routes>
-      <Route path="/" element={<Homepage />} />
+      <Route path="/" element={<Landing />} />
+      <Route path="/home" element={<Homepage />} />
       <Route path="/details" element={<ServiceDetails />} />
       <Route path="/services" element={<Services />} />
       <Route path="/add-service" element={<AddService />} />
