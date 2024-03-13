ad827fcec6542180632e6e69e2fc04aa250d1c07
Othman Salahi <132019758+OthmanSALAHI@users.noreply.github.com>
2024-03-13 14:44:46 +0000

    Update Routes.jsx

--- frontend/src/components/routes/Routes.jsx
+++ frontend/src/components/routes/Routes.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Signin from '../Auth/Signin';
-import Signup from '../Auth/Signup';
-import ForgotPassword from '../Auth/ForgotPassword';
-import Services from '../pages/Services';
-import Homepage from '../pages/Homepage';
-import ServiceDetails from '../pages/ServiceDetails';
-import { Profilepage } from '../pages/ProfilePage';
-import AddService from '../pages/AddService';
-import SettingProfile from '../pages/SettingProfile';
-import TypePage from '../Auth/TypePage';
-import AboutUs from '../pages/AboutUs';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Signin from "../Auth/Signin";
+import Signup from "../Auth/Signup";
+import ForgotPassword from "../Auth/ForgotPassword";
+import Services from "../pages/Services";
+import Homepage from "../pages/Homepage";
+import ServiceDetails from "../pages/ServiceDetails";
+import { Profilepage } from "../pages/ProfilePage";
+import AddService from "../pages/AddService";
+import SettingProfile from "../pages/SettingProfile";
+import { Landing } from "../pages/Landing";
 
 const Routers = () => {
   return (
     <Routes>
-      <Route path="/" element={<Homepage />} />
+      <Route path="/" element={<Landing />} />
+      <Route path="/home" element={<Homepage />} />
       <Route path="/details" element={<ServiceDetails />} />
       <Route path="/services" element={<Services />} />
       <Route path="/add-service" element={<AddService />} />
@@ -24,8 +24,6 @@ const Routers = () => {
       <Route path="/forgotpassword" element={<ForgotPassword />} />
       <Route path="/profile" element={<Profilepage />} />
       <Route path="/profile-setting" element={<SettingProfile />} />
-      <Route path="/type-user" element={<TypePage />} />
-      <Route path="/about-us" element={<AboutUs />} />
     </Routes>
   );
 };
